1c87b5d5d9681493afbb370ba392c068de59b0b6
moonpiedumplings <jeff.fonseca777@gmail.com>
2023-03-31 13:38:43 -0700

    container.nix starts

---
+++ container.nix
@@ -0,0 +1,27 @@
+containers.demo = {
+  extra = {
+    # Sets
+    # privateNetwork = true
+    # hostAddress = "${addressPrefix}.1"
+    # localAddress = "${addressPrefix}.2"
+    addressPrefix = "10.250.0";
+
+    # Enable internet access for the container
+    enableWAN = true;
+    # Always allow connections from hostAddress
+    firewallAllowHost = true;
+    # Make the container's localhost reachable via localAddress
+    exposeLocalhost = true;
+  };
+  config = { pkgs, ... }: {
+      systemd.services.hello = {
+        wantedBy = [ "multi-user.target" ];
+        script = ''
+          while true; do
+            echo hello | ${pkgs.netcat}/bin/nc -lN 50
+          done
+        '';
+      };
+      networking.firewall.allowedTCPPorts = [ 50 ];
+  }
+};
